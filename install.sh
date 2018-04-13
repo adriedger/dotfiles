d28d6d83ad03b64656a427540e78eca3e23d9e9a
andre <adriedge@gmail.com>
2018-04-13 14:21:29 -0600

    removed gconfig

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # To get vim 8.0 -> sudo add-apt-repository ppa:jonathonf/vim
 # for windows copy this: LS_COLORS=$LS_COLORS:'ow=1;34:' ; export LS_COLORS
 
-cp -r .vim/* ~/.vim/
+cp -r .vim/ ~
 cp .vimrc .inputrc ~
-echo "export GOPATH=$HOME/.go" >> .bashrc
-echo "alias stu='ssh driedgera2@students.cs.macewan.ca'" >> .bashrc
-echo "alias sql='psql -U postgres'" >> .bashrc
+#echo "export GOPATH=$HOME/.go" >> .bashrc
+echo "alias stu='ssh driedgera2@students.cs.macewan.ca'" >> ~/.bashrc
+echo "alias sql='psql -U postgres'" >> ~/.bashrc
